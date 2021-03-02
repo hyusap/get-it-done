e5a4656ac10faca02cae5e8b053e6df8b2eec5fa
Ayush <61765149+citricacid2@users.noreply.github.com>
2021-03-02 12:22:36 -0500

    Added the base for most files

--- database/sql.py
+++ database/sql.py
@@ -5,7 +5,7 @@ from sqlite3 import Error
 class database:
     connection = None
 
-    def __init__(self, path):
+    def __init__(self, path='database/main.db'):
         try:
             self.connection = sqlite3.connect(path)
             print("Connection to SQLite DB successful")
@@ -21,12 +21,21 @@ class database:
         except Error as e:
             print(f"The error '{e}' occurred")
 
-    def read(self, query):
+    def read(self, *args):
         cursor = self.connection.cursor()
-        result = None
         try:
-            cursor.execute(query)
+            cursor.execute(*args)
             result = cursor.fetchall()
             return result
         except Error as e:
             print(f"The error '{e}' occurred")
+
+    def close(self):
+        self.connection.close()
+
+
+def db_read(*args):
+    db = database('database/main.db')
+    data = db.read(*args)
+    db.close()
+    return data
