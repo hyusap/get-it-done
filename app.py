cd8e80327c61984ec5d625e1cfef257d34cde14d
Ayush <61765149+citricacid2@users.noreply.github.com>
2021-03-02 12:22:36 -0500

    Added the base for most files

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
-import sql
+from database.sql import database, db_read
 
 app = Flask(__name__)
-db = sql.database('main.db')
 
 
 @app.route('/')
@@ -12,7 +11,34 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    data = db_read('select name, plaintext from topics')
+    return render_template('home.html', data=data)
+
+
+@app.route('/home/<topic>')
+def topics(topic):
+    data = db_read(
+        'select courses.name, t.plaintext, courses.plaintext from courses join topics t on t.id = courses.topic_id where t.plaintext = ?',
+        [topic]
+    )
+    print(data)
+    return render_template('topics.html', data=data)
+
+
+@app.route('/home/<topic>/<course>')
+def courses(topic, course):
+    data = db_read(
+        """
+        select t.first_name, t.last_name, classes.period from classes
+            join teachers t on t.id = classes.teacher_id
+            join courses c on c.id = classes.course_id
+            join topics t2 on t2.id = c.topic_id
+            where t2.plaintext = ? and c.plaintext = ?
+        """,
+        [topic, course]
+    )
+    print(data)
+    return render_template('courses.html', data=data)
 
 
 if __name__ == '__main__':
--- database/schemas.sql
+++ database/schemas.sql
@@ -1,11 +1,18 @@
-# Get it DONE!
-A crowd sourced homework management app!
-
-### SQL Schemas
-```sqlite
 create table teachers (id INTEGER, first_name TEXT, last_name TEXT, PRIMARY KEY(id));
 
-create table courses (id INTEGER, name TEXT, PRIMARY KEY(id));
+create table topics (
+    id INTEGER,
+    name TEXT,
+    PRIMARY KEY (id)
+);
+
+create table courses (
+    id INTEGER,
+    name TEXT,
+    topic_id INTEGER,
+    PRIMARY KEY(id),
+    FOREIGN KEY (topic_id) REFERENCES topics(id)
+);
 
 create table classes (
     id INTEGER,
@@ -16,4 +23,3 @@ create table classes (
     FOREIGN KEY (course_id) REFERENCES courses(id),
     FOREIGN KEY (teacher_id) REFERENCES teachers(id)
 );
-```
