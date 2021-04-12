8509c1591bde411ce6d8a4056d94ad761475b89b
citricacid2 <paulayush@gmail.com>
2021-04-12 13:48:54 -0400

    create.py and navigation.py rebased

--- blueprints/create.py
+++ blueprints/create.py
@@ -1,13 +1,6 @@
 from flask import Blueprint, request, render_template, flash, redirect
 # from database.sql import db_read, db_execute
-
-
-def db_read(*args):
-    return 'hey'
-
-
-def db_execute(*args):
-    return 'hey'
+from database.sql import Topics, Courses, Teachers, Classes, Work
 
 
 create = Blueprint('create', __name__)
@@ -21,15 +14,12 @@ def create_index():
 @create.route('/course', methods=["GET", "POST"])
 def make_course():
     if request.method == 'GET':
-        data = db_read('select name, id from topics')
+        data = Topics.select()
         return render_template('creation/course.html', data=data)
     else:
         name = request.form.get('name')
         id = request.form.get('under')
-        db_execute(
-            'insert into courses (name, topic_id, plaintext) values (?, ?, ?)',
-            [name, id, name.replace(" ", "").lower()]
-        )
+        Courses.create(name=name, topic=id, plaintext=name.replace(" ", "").lower())
         flash(f"You created course \"{name}\" successfully! Please wait for it to be approved by an admin!")
         return redirect('/home')
 
@@ -41,10 +31,7 @@ def make_teacher():
     else:
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
-        db_execute(
-            'insert into teachers (first_name, last_name) values (?, ?)',
-            [firstname, lastname]
-        )
+        Teachers.create(first_name=firstname, last_name=lastname)
         flash(f"You created teacher \"{firstname} {lastname}\" successfully!")
         return redirect('/home')
 
@@ -52,17 +39,14 @@ def make_teacher():
 @create.route('/class', methods=["GET", "POST"])
 def make_class():
     if request.method == 'GET':
-        teachers = db_read('select id, first_name, last_name from teachers')
-        courses = db_read('select id, name from courses')
+        teachers = Teachers.select()
+        courses = Courses.select()
         return render_template('creation/class.html', teachers=teachers, courses=courses)
     else:
         course = request.form.get('course')
         teacher = request.form.get('teacher')
         period = request.form.get('period')
-        db_execute(
-            'insert into classes (course_id, teacher_id, period) values (?, ?, ?)',
-            [course, teacher, period]
-        )
+        Classes.create(course=course, teacher=teacher, period=period)
         flash(f"You created the class successfully! Please wait for it to be approved by an admin!")
         return redirect('/home')
 
@@ -78,10 +62,7 @@ def make_item():
         url = request.form.get('url')
         date = request.form.get('date')
 
-        db_execute(
-            'insert into work (class_id, name, description, url, due_by) values (?, ?, ?, ?, ?)',
-            [class_id, name, description, url, date]
-        )
+        Work.create(from_class=class_id, name=name, description=description, url=url, due_by=date)
 
         flash(f"You created the item successfully!")
         return redirect('/home')
